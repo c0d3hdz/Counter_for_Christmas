6c8717ead5adefe1ffdfcc3b1b4371a9f117527a
c0d3hdz <marquezhernandez.damian@gmail.com>
2024-12-11 21:46:48 -0600

    optimizacion

--- worker.js
+++ worker.js
@@ -1,24 +1,25 @@
 // worker.js
 onmessage = event => {
-    const { duration } = event.data;
-    const now = new Date();
-    let navidad = new Date(now.getFullYear(), 11, 25).getTime();
+    const { duration } = event.data //ya no se usa pero aun no logro eliminarlo
+    const now = new Date()
+    let navidad = new Date(now.getFullYear(), 11, 25).getTime()
 
     if (now.getTime() > navidad) {
-        navidad = new Date(now.getFullYear() + 1, 11, 25).getTime();
+        navidad = new Date(now.getFullYear() + 1, 11, 25).getTime()
     }
 
     const updateCountdown = () => {
-        const now = new Date().getTime();
-        const distance = navidad - now;
-        const days = Math.floor(distance / (1000 * 60 * 60 * 24));
-        const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-        const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+        const now = new Date().getTime()
+        const distance = navidad - now
+        const days = Math.floor(distance / (1000 * 60 * 60 * 24))
+        const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+        const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60))
+        const seconds = Math.floor((distance % (1000 * 60)) / 1000)
 
-        postMessage({ days, hours, minutes, seconds });
-    };
+        postMessage({ days, hours, minutes, seconds })
 
-    updateCountdown();
-    setInterval(updateCountdown, duration);
+        requestAnimationFrame(updateCountdown)
+    }
+
+    updateCountdown()
 }
